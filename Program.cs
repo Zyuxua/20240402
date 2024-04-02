a25c625b99c9f9e1cb5a928390bf81e5861c37a0
曾羽瑄 <03147012cy@gmail.com>
2024-04-02 11:50:13 +0800

    v3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");//
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, ntust!");//V2
+Console.WriteLine("Hello, NTUST!");//V3
+
